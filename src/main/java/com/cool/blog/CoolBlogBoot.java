579d133761f8aa854605fe32326c2051ee9a6b13
vincent <1341870251@qq.com>
2018-12-27 11:46:20 +0800

    [cool-blog] 初始化

---
+++ src/main/java/com/cool/blog/CoolBlogBoot.java
@@ -0,0 +1,14 @@
+package com.cool.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolBlogBoot {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoolBlogBoot.class, args);
+    }
+
+}
+
